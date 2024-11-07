ba2c9f6c2e9dfbfa92bbb8264e38df693bec4853
Mehedi-Hasan-18 <150595249+Mehedi-Hasan-18@users.noreply.github.com>
2024-11-08 02:01:36 +0800

    Update C_1_Powering_the_Hero_easy_version.cpp

--- WEEK 02/Day 05/C_1_Powering_the_Hero_easy_version.cpp
+++ WEEK 02/Day 05/C_1_Powering_the_Hero_easy_version.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
 signed main()
 {
     int t;
@@ -15,7 +14,7 @@ signed main()
         {
             cin >> v[i];
         }
-        int ans = 0;
+        long long int ans = 0;
         for (int i = 0; i < n; i++)
         {
             if (v[i] == 0)
