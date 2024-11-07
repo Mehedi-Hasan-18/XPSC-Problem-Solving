d1c9b87ba63cfeaefabf30ebc89bb2fc0b18ab97
Mehedi-Hasan-18 <150595249+Mehedi-Hasan-18@users.noreply.github.com>
2024-11-08 01:59:09 +0800

    Update C_1_Powering_the_Hero_easy_version.cpp

--- WEEK 02/Day 05/C_1_Powering_the_Hero_easy_version.cpp
+++ WEEK 02/Day 05/C_1_Powering_the_Hero_easy_version.cpp
@@ -1,35 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+#define int long long
+signed main()
+{
     int t;
     cin >> t;
-    while(t--){
+    while (t--)
+    {
         int n;
         cin >> n;
-        priority_queue<int>pq;
-        vector<int>v(n+1);
-        for(int i=0;i<n;i++){
+        priority_queue<int> pq;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
             cin >> v[i];
         }
         int ans = 0;
-        for(int i=0;i<n;i++){
-            if(v[i] == 0){
-                if(!pq.empty()){
+        for (int i = 0; i < n; i++)
+        {
+            if (v[i] == 0)
+            {
+                if (pq.size() > 0)
+                {
                     ans += pq.top();
                     pq.pop();
                 }
             }
-            else{
+            else
+            {
                 pq.push(v[i]);
             }
         }
         cout << ans << endl;
-        
     }
-
-    
-    return 0;
 }
