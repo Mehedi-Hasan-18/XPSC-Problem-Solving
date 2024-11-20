1e3d5627d2e182695759c19c0a380f57afa96e8d
Mehedi-Hasan-18 <150595249+Mehedi-Hasan-18@users.noreply.github.com>
2024-11-21 01:01:22 +0800

    Add files via upload

---
+++ Week 04/Day 04/B_Segment_with_Big_Sum.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    long long k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    int l = 0, r = 0;
+    int ans = INT_MAX;
+    long long sum = 0;
+    while (r < n)
+    {
+        sum += v[r];
+        while (sum >= k)
+        {
+            ans = min(ans, r - l + 1);
+            sum -= v[l];
+            l++;
+        }
+        r++;
+    }
+    if(ans == INT_MAX) cout <<"-1\n";
+    else cout << ans << endl;
+
+    return 0;
+}
